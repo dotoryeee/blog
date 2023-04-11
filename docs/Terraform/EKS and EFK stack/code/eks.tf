b8cf2c289a2d5890cc2b5d56808d807c062c9868
dotoryeee <bayer@kakao.com>
2023-04-11 13:27:53 +0900

    deploy EKS and EFK stack with terraform

---
+++ docs/Terraform/EKS and EFK stack/code/eks.tf
@@ -0,0 +1,36 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "17.0.0"
+
+  cluster_name = "eks"
+  cluster_version = 1.25
+  subnets      = module.vpc.private_subnets
+
+  tags = {
+    Terraform = "true"
+    Project   = "EKS"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  node_groups_defaults = {
+    ami_type  = "AL2_x86_64"
+    disk_size = 30
+  }
+
+  node_groups = {
+    master = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+      instance_type    = "t2.micro"
+    }
+    data = {
+      desired_capacity = 3
+      max_capacity     = 3
+      min_capacity     = 3
+      instance_type    = "t2.micro"
+    }
+  }
+}
+
