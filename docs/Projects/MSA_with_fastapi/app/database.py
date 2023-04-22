a8ee7c01d04753c09644e2772a89332302f31c1a
dotoryeee <bayer@kakao.com>
2023-04-23 08:27:50 +0900

    post server complete

--- docs/Projects/MSA_with_fastapi/app/database.py
+++ docs/Projects/MSA_with_fastapi/app/database.py
@@ -27,10 +27,5 @@ def initialize():
         logger.debug("creating: new table")
         Base.metadata.create_all(engine)
     except:
-        logger.warn(f"warning: table already exists" )
+        logger.info("skip table creation: table already exists" )
 
-"""
-최신 버전의 sqlalchemy에서는 bind대신 bind_to를 사용하고, metadata대신 metadata=None을 사용
-따라서 metadata의 bind 메서드와 engine을 연결
-declarative_base 함수는 metadata 인수에 metadata=None을 사용하여 metadata객체를 인수로 받도록 수정
-"""
