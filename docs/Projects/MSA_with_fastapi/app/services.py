e565e53640a526b18459144b2a7c92d1cc24990b
dotoryeee <bayer@kakao.com>
2023-04-23 01:41:27 +0900

    add logger, update front

--- docs/Projects/MSA_with_fastapi/app/services.py
+++ docs/Projects/MSA_with_fastapi/app/services.py
@@ -2,35 +2,42 @@ from sqlalchemy.orm import Session
 from . import models
 from .models import Post
 from .database import SessionLocal
+import logging
 
-"""
-TypeError: 'generator' object does not support the context manager protocol
--> get_db 호출하는 쪽에서 async with 구문으로 호출해서 사용하면 된다
-"""
+logger = logging.getLogger("post_server_logger")
 
 def get_db():
     """
-    get_db 호출하는 쪽에서 with(x) -> async with 구문으로 호출해서 사용하면 된다
+    get_db 호출하는 쪽에서 with 구문으로 호출해서 사용하면 된다
     """
+    logger.debug("creating: new DB connection")
     db = SessionLocal() #SessionLocal은 sessionmaker 인스턴스
-    try:
-        yield db
-    finally: #호출하는 쪽의 with 구문이 종료되면 finally구문이 실행됨(db session 종료)
-        db.close()
+    logger.debug("created: new DB connection")
+    # try:
+    #     yield db 적용 실패
+    # finally: #호출하는 쪽의 with 구문이 종료되면 finally구문이 실행됨(db session 종료)
+    #     logger.debug("closing DB connection")
+    #     db.close()
+    return db
 
-async def get_all_posts():
+def get_all_posts():
     """
     게시글 전체 조회를 위한 함수
     """
-    async with get_db() as db:
+    logger.debug("trying: get all posts from DB")
+    with get_db() as db:
+        logger.debug("created: get all posts")
         return db.query(models.Post).all()
 
-async def create_post(post: Post):
+def create_post(post: Post):
     """
     게시글 생성을 위한 함수
     """
-    async with get_db() as db:
+    with get_db() as db:
+        logger.debug("created: insert new post to DB")
         db.add(post)
-        await db.commit()
-        await db.refresh(post)
+        db.commit()
+        logger.debug("return: db.commit")
+        db.refresh(post)
+        logger.debug("return: db.refresh")
         return post
