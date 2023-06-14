844783434a63c11a27453d9490cd0cf7a7a35245
dotoryeee <bayer@kakao.com>
2023-06-14 13:20:57 +0900

    Deployed 3d9f98f with MkDocs version: 1.4.3

--- Memozzang/nhn-pt/script.sh
+++ Memozzang/nhn-pt/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # install nginx
 sudo apt update && sudo apt install nginx -y
-sudo wget https://kr2-api-object-storage.nhncloudservice.com/v1/AUTH_cca4d757fedb477a91cc757b336ce430/garden_public/nginx.conf -O /etc/nginx/nginx.conf
+sudo wget https://kr2-api-object-storage.nhncloudservice.com/v1/AUTH_**************/garden_public/nginx.conf -O /etc/nginx/nginx.conf
 sudo nginx -s reload
 echo '<h1>This page created for Log & Crash service test</h1>' | sudo tee /var/www/html/index.html
 # install logstash
@@ -10,6 +10,6 @@ sudo apt-get install apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install logstash
 sudo usermod -aG adm logstash
-sudo wget https://kr2-api-object-storage.nhncloudservice.com/v1/AUTH_cca4d757fedb477a91cc757b336ce430/garden_public/nhn-log-crash.conf -O /etc/logstash/conf.d/nhn-log-crash.conf
+sudo wget https://kr2-api-object-storage.nhncloudservice.com/v1/AUTH_**************/garden_public/nhn-log-crash.conf -O /etc/logstash/conf.d/nhn-log-crash.conf
 sudo systemctl enable logstash; sudo systemctl restart logstash
 
